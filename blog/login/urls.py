68ebf686083299423174a7c34cd7766a0e77f9f5
760730895 <760730895@qq.com>
2018-12-29 15:59:07 +0800

    first commit

---
+++ blog/login/urls.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 18-12-27 下午3:32
+# @Author  : hucl
+"""Defines url patterns for login."""
+
+from django.conf.urls import url
+
+from . import views
+
+app_name = 'login'
+urlpatterns = [
+    # Home page.
+    url(r'^$', views.index, name='index'),
+
+    # Show all topics.
+    url(r'^topics/$', views.topics, name='topics'),
+
+    # Detail page for a single topic.
+    url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
+
+    # Page for adding a new topic.
+    url(r'^new_topic/$', views.new_topic, name='new_topic'),
+
+    # Page for adding a new entry.
+    url(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry, name='new_entry'),
+
+    # Page for editing an entry.
+    url(r'^edit_entry/(?P<entry_id>\d+)/$', views.edit_entry,
+        name='edit_entry'),
+]
